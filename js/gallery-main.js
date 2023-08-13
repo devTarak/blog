769331ccd1bf155a0ce983e1d3622d05ea889d86
Tarak Rahman <101055758+devTarak@users.noreply.github.com>
2023-08-14 04:03:03 +0600

    Update gallery-main.js

--- js/gallery-main.js
+++ js/gallery-main.js
@@ -1,15 +1,15 @@
-const images = document.querySelectorAll('.image_main img');
-const lightbox = document.querySelector('.lightbox_main');
-const lightboxImg = lightbox.querySelector('img');
-const closeButton = document.querySelector('.close-button_main');
+const images_main = document.querySelectorAll('.image_main img');
+const lightbox_main = document.querySelector('.lightbox_main');
+const lightboxImg_main = lightbox_main.querySelector('img');
+const closeButton_main = document.querySelector('.close-button_main');
 
-images.forEach(image => {
-  image.addEventListener('click', () => {
-    lightboxImg.src = image.src;
-    lightbox.style.display = 'flex';
+images_main.forEach(image_main => {
+  image_main.addEventListener('click', () => {
+    lightboxImg_main.src = image_main.src;
+    lightbox_main.style.display = 'flex';
   });
 });
 
-closeButton.addEventListener('click', () => {
-  lightbox.style.display = 'none';
+closeButton_main.addEventListener('click', () => {
+  lightbox_main.style.display = 'none';
 });
