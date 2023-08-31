0d062cc503e19bce75e8afe03e93274b074fe925
Tarak Rahman <101055758+devTarak@users.noreply.github.com>
2023-08-31 16:16:39 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,3 +9,8 @@ window.onload = function() {
     const logoImage = document.querySelector('.Main_Logo_1');
     logoImage.classList.add('show');
 };
+//for mobile
+window.onload = function() {
+    const logoImagemB = document.querySelector('.Main_Logo_mb_1');
+    logoImagemB.classList.add('show_mb');
+};
