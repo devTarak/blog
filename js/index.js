19fbf3a0b1c95dd687bb977b905d3c9a68e7d18a
Tarak Rahman <101055758+devTarak@users.noreply.github.com>
2023-08-31 16:30:20 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,13 +4,10 @@ const menuButton = document.querySelector('.MenuButton');
         menuButton.addEventListener('click', () => {
             mobileNavMenu.classList.toggle('active');
         });
-// This example uses window.onload to add the 'show' class after the page loads
 window.onload = function() {
     const logoImage = document.querySelector('.Main_Logo_1');
     logoImage.classList.add('show');
-};
-//for mobile
-window.onload = function() {
+
     const logoImagemB = document.querySelector('.Main_Logo_mb_1');
     logoImagemB.classList.add('show_mb');
 };
