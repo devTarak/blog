0a672abdb38e8220b2128dfed49dd75610a9f4e8
Tarak Rahman <101055758+devTarak@users.noreply.github.com>
2023-09-10 22:58:04 +0600

    ui updated

--- js/index.js
+++ js/index.js
@@ -11,3 +11,12 @@ window.onload = function() {
     const logoImagemB = document.querySelector('.Main_Logo_mb_1');
     logoImagemB.classList.add('show_mb');
 };
+var myThemeIcon = document.getElementById("myThemeIcon");
+myThemeIcon.onclick = function(){
+    document.body.classList.toggle("light-Theme");
+    if(document.body.classList.contains("light-Theme")){
+        myThemeIcon.src = "img/moon.png";
+    }else{
+        myThemeIcon.src = "img/sun.png";
+    }
+}
